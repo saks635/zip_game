4e43ec867e993b87110ee009aa4158cb85f5a59e
Saksham <your_email@example.com>
2025-10-22 22:37:05 +0530

    add scoring fature

--- zip_game.py
+++ zip_game.py
@@ -2,13 +2,16 @@ import random
 
 print("🎮 Welcome to the Zip Game!")
 secret = random.randint(1, 10)
+attempts = 0
 
 while True:
     guess = int(input("Guess a number (1–10): "))
+    attempts += 1
     if guess == secret:
-        print("✅ You Win!")
+        print(f"✅ You Win in {attempts} attempts!")
         break
     elif guess < secret:
         print("⬆️ Too Low! Try again.")
     else:
         print("⬇️ Too High! Try again.")
+
